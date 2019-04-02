32bf699bd803bc2e3a9a5625c5da8ef0e057df1e
hoang <hoang@DESKTOP-TD02PEN>
2019-04-03 00:15:58 +0700

    initial commit.

---
+++ src/bai2/mathEx.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai2;
+
+/**
+ *
+ * @author hoang
+ */
+
+public class mathEx{
+    public static void main(String[] args) {
+        System.out.println("sin(45)= " +Math.sin(Math.toRadians(45)));
+        System.out.println("45.66 sau khi duoc lan tron: "+Math.round(45.66));
+         System.out.println("45.66 sau khi duoc lan tron: "+Math.floor(45.66));
+         
+    }
+}
